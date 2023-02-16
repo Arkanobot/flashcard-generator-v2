fe2c8760ca54e566bf7a0a2d2adc566aa4028da3
Shreyas Bhat K <67960703+Arkanobot@users.noreply.github.com>
2023-02-16 22:10:48 +0530

    FlashCardsList Test Complete

--- src/__test__/FlashcardsList.test.js
+++ src/__test__/FlashcardsList.test.js
@@ -2,33 +2,38 @@ import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import FlashcardsList from "../components/MyFlashcards/FlashcardsList";
 import { Provider } from "react-redux";
-// import store from "../redux/store";
 import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "../redux/flashcards";
 
+const preloadedState = {
+  cards: {
+    cards: [
+      {
+        values: {
+          cardName: "A Test Name",
+          cardDesc: "A Test card description",
+          cardImg: "https://unsplash.com/photos/fXghdlO6mgk",
+          terms: [{ termName: "Test Term1" }, { termName: "Test Term2" }],
+        },
+      },
+    ],
+  },
+};
+
+const store = configureStore({
+  reducer: {
+    cards: cardReducer,
+  },
+  preloadedState,
+});
+console.log(`Hello World!`, preloadedState);
+
 describe("Flash cards list", () => {
   it("Should show title cards when there is value in the store", () => {
-    const store = configureStore({
-      reducer: {
-        cards: cardReducer,
-      },
-      preloadedState: {
-        cards: [
-          {
-            values: {
-              cardName: "A Test Name",
-              cardDesc: "A Test card description",
-              cardImg: "https://unsplash.com/photos/fXghdlO6mgk",
-              terms: [{ termName: "Test Term1" }, { termName: "Test Term2" }],
-            },
-          },
-        ],
-      },
-    });
     render(
       <BrowserRouter>
         <Provider store={store}>
-          <FlashcardsList />
+          <FlashcardsList i={1} />
         </Provider>
       </BrowserRouter>
     );
--- src/components/MyFlashcards/FlashcardsList.jsx
+++ src/components/MyFlashcards/FlashcardsList.jsx
@@ -4,7 +4,6 @@ import TitleCard from "./TitleCard";
 
 export default function FlashcardsList() {
   const cards = useSelector((state) => state.cards.cards); // fetching data from cards store for displaying the cards
-  console.log(cards);
   return (
     <div className="px-5 my-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
